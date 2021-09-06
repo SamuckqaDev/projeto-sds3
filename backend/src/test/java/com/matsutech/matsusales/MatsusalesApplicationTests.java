d3a60c164e336ab82c80506470aa9d5005110ca3
Samuel Adriano Tomimatu <samuel.tomimatu@bandtec.com.br>
2021-09-06 18:53:48 -0300

    Created project 'spring react'

---
+++ backend/src/test/java/com/matsutech/matsusales/MatsusalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matsutech.matsusales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatsusalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
